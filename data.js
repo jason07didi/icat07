caa22986d7cc3bb20d4ca979715e3ff003b1332f
iCat <128765796+jason07didi@users.noreply.github.com>
2026-06-15 10:31:19 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,34 +4,34 @@ const datasets = [
     category: "行政区划",
     format: "SHP / GeoJSON",
     description: "包含省、市、区县尺度行政边界数据，适用于GIS制图、空间统计和城市规划分析。",
-    link: "https://pan.baidu.com/"
+    link: "https://pan.baidu.com/s/1VkDJF3x-xT6jepfxz6KbeQ?pwd=pfw8"
   },
   {
     title: "土地利用 / 土地覆盖数据",
     category: "土地利用",
-    format: "TIF / SHP",
+    format: "TIF",
     description: "包括建设用地、绿地、水体、农田等空间数据，可用于城市空间分析、生态评价和用地变化研究。",
-    link: "https://pan.baidu.com/"
+    link: "https://pan.baidu.com/s/1jniTwVOV378_dqERFohqyA?pwd=m3rv"
   },
   {
     title: "DEM数字高程模型",
     category: "地形数据",
     format: "TIF",
     description: "常用数字高程模型数据，可用于坡度坡向、水文分析、地形因子提取等。",
-    link: "https://pan.baidu.com/"
+    link: "https://pan.baidu.com/s/1CtoTtGQodWz_53C4QGf7cQ?pwd=3uu2"
   },
   {
-    title: "遥感影像数据资源",
-    category: "遥感数据",
-    format: "TIF",
-    description: "整理Landsat、Sentinel等遥感影像数据资源，适用于城市扩张、生态环境和地表特征分析。",
-    link: "https://pan.baidu.com/"
+    title: "城市用地功能",
+    category: "城市规划",
+    format: "SHP",
+    description: "居住用地、商业用地、工业用地分类等。",
+    link: "https://pan.baidu.com/s/1rGxu2lPlgF6HsNZ3fXE6ZQ?pwd=c8u8"
   },
   {
     title: "城市道路与建筑数据",
     category: "城市空间",
     format: "SHP / GeoJSON",
     description: "城市道路、建筑轮廓、路网等基础空间数据，适用于城市形态、可达性和空间结构分析。",
-    link: "https://pan.baidu.com/"
+    link: "https://pan.baidu.com/s/1ec2QRRqrU2CGVaB8LPKCsQ?pwd=vnxb"
   }
 ];
